8d10a3eaa6f6e9f33f84f5b6f6496afee190456e
barbaraalm <63905110+barbaraalm@users.noreply.github.com>
2020-05-30 13:31:08 +0200

    Update Risk.py

--- Risk.py
+++ Risk.py
@@ -118,26 +118,26 @@ def port_perf_data(value):
                     'titlefont': dict(
                         family = 'verdana',
                         size = 24,
-                    'xaxis': dict(
-                        rangeselector=dict(
-                            buttons=list([
-                                dict(count=1,
-                                    label='1m',
-                                    step='month',
-                                    stepmode='backward'),
-                                dict(count=6,
-                                    label='6m',
-                                    step='month',
-                                    stepmode='backward'),
-                                dict(count=1,
-                                    label='YTD',
-                                    step='year',
-                                    stepmode='todate'),
-                                dict(count=1,
-                                    label='1y',
-                                    step='year',
-                                    stepmode='backward'),
-                                dict(step='all')
+                        'xaxis': dict(
+                            rangeselector=dict(
+                                buttons=list([
+                                    dict(count=1,
+                                         label='1m',
+                                         step='month',
+                                         stepmode='backward'),
+                                    dict(count=6,
+                                        label='6m',
+                                        step='month',
+                                        stepmode='backward'),
+                                    dict(count=1,
+                                        label='YTD',
+                                        step='year',
+                                        stepmode='todate'),
+                                    dict(count=1,
+                                        label='1y',
+                                        step='year',
+                                        stepmode='backward'),
+                                    dict(step='all')
                                 ])
                             )
                         )
