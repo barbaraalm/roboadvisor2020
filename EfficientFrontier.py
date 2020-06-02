c68c5b9a7da053cac291c2ed31a812e286f72116
barbaraalm <63905110+barbaraalm@users.noreply.github.com>
2020-06-02 17:20:55 +0200

    Update EfficientFrontier.py

--- EfficientFrontier.py
+++ EfficientFrontier.py
@@ -169,7 +169,8 @@ def plot_portfolios(rel_risk_aversion, X, Y,p_perf):
         #width= 1000,
         xaxis_title='Volatility',
         yaxis_title='Return',
-        #legend_orientation="h",   
+        legend=dict( y=-0.2),
+        legend_orientation="h",   
     )
     fig_frontier = Figure( layout=layout)
     fig_frontier.add_trace(
